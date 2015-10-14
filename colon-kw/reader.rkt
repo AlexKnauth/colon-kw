dd68a43259deafe1f967e1ed22f6f41c16f4120a
AlexKnauth <alexander@knauth.org>
2015-10-14 10:51:35 -0400

    better error checking

--- colon-kw/reader.rkt
+++ colon-kw/reader.rkt
@@ -2,15 +2,22 @@
 
 (provide make-colon-keyword-readtable)
 
+(require syntax/readerr)
+
 ;; from:
 ;; https://groups.google.com/d/msg/racket-dev/HUXsCRI5ab0/2jdn9FTVCwAJ
 
 (define read-colon-keyword
   (case-lambda
     [(ch port)
-     (symbol->keyword (read/recursive port))]
+     (define sym (read/recursive port))
+     (unless (symbol? sym)
+       (raise-read-error "bad :keyword syntax" (object-name port) #f #f #f #f))
+     (symbol->keyword sym)]
     [(ch port src line col pos)
      (define stx (read-syntax/recursive src port))
+     (unless (symbol? (syntax-e stx))
+       (raise-read-error "bad :keyword syntax" src line col pos (add1 (syntax-span stx))))
      (datum->syntax #f (symbol->keyword (syntax-e stx))
                     (list src line col pos (add1 (syntax-span stx)))
                     stx)]))
